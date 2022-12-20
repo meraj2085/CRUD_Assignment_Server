2e0a785748c102ec0c52cdf19f8db9be4e250d11
meraj2085 <samirmeraj60@gmail.com>
2022-12-20 20:03:08 +0600

    Adding /users api for getting all data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -17,10 +17,18 @@ async function run(){
           const UsersCollection = client.db("CRUD_Assignment").collection("users");
 
           app.put('/user', async (req, res) => {
-               const newUSer = {name: 'Meraj', email: 'samirmeraj60@gmail.com'}
+               const newUSer = req.body;
                const result = await UsersCollection.insertOne(newUSer)
                res.send(result)
           })
+
+          app.get('/users', async(req, res)=>{
+               const query = {};
+               const result = await UsersCollection.find(query).toArray();
+               res.send(result)
+          })
+
+          
      }
      finally{}
 }
