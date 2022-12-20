df6208089fa5848947989261ed32b6a128d14e9e
meraj2085 <samirmeraj60@gmail.com>
2022-12-20 20:03:42 +0600

    Adding /users/:id api for deleting data

--- index.js
+++ index.js
@@ -28,7 +28,12 @@ async function run(){
                res.send(result)
           })
 
-          
+          app.delete("/users/:id", async (req, res) => {
+               const id = req.params.id;
+               const filter = { _id: ObjectId(id) };
+               const result = await UsersCollection.deleteOne(filter);
+               res.send(result);
+          });
      }
      finally{}
 }
